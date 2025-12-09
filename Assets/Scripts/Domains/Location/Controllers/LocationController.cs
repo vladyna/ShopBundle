ec1cdbc3af6497a63a8c90013005bd5b6b518088
Vladyslav Stepura <svladyna@gmail.com>
2025-12-09 18:42:50 +0200

    Updated location apply logic

--- Assets/Scripts/Domains/Location/Controllers/LocationController.cs
+++ Assets/Scripts/Domains/Location/Controllers/LocationController.cs
@@ -2,6 +2,7 @@ using Core.Abstractions;
 using Core.PlayerData;
 using Domains.Location.Actions;
 using Domains.Location.Models;
+using System;
 
 namespace Domains.Location.Controllers
 {
@@ -20,13 +21,18 @@ namespace Domains.Location.Controllers
 
         public override bool CanApply(BundleAction action)
         {
-            return true;
+            if (action is SetLocationSO slSo)
+            {
+                var currentValue = PlayerData.Instance.Get(_key);
+                return currentValue.Equals(slSo.Value, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
         }
 
         public override void ApplyPrice(BundleAction action)
         {
             if (action is SetLocationSO gAction)
-                ModifyLocation(gAction.Value);
+                ModifyLocation(Key.DefaultValue);
         }
 
         public override void ApplyReward(BundleAction action)
